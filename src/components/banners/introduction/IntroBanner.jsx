018caa4ce11bd70c789671f661c988dcec7aa4da
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2022-05-23 01:02:35 +0530

    added: intro banner

--- src/components/banners/introduction/IntroBanner.jsx
+++ src/components/banners/introduction/IntroBanner.jsx
@@ -1,7 +1,23 @@
-import React from "react"
+import React from "react";
+import "./IntroBanner.css";
 
 export const IntroBanner = () => {
-    return <>
-        <h1>This is IntroBanner</h1>
-    </>
-}
+  return (
+    <div className="intro-banner">
+      <div className="intro-content">
+        <h1>Not your average banking experience.</h1>
+        <div>
+          <p>Banking, but seasoned with SALT.</p>
+          <p>
+            We deal with all things related to global business banking so that
+            you don't have to.
+          </p>
+        </div>
+        <button>Get Started</button>
+      </div>
+      <div className="intro-hero">
+        <img src="./assets/hero.svg" alt="introhero" />
+      </div>
+    </div>
+  );
+};
