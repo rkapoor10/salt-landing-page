58bb65470cd09482d7baba82c850eac063aebfb0
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2022-05-24 11:48:40 +0530

    refactor: subscription card

--- src/components/cards/subscriptionCard/SubscriptionCard.jsx
+++ src/components/cards/subscriptionCard/SubscriptionCard.jsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
+import "./SubscriptionCard.css";
 
 export const SubscriptionCard = () => {
-    return <>
-        <h1>This is SubscriptionCard</h1>
-    </>
-}
+  return (
+    <div className="flex flex-center subscription-card ">
+      <div className="flex-col">
+        <h1>Subscribe Now for Get Special Features!</h1>
+        <p>Banking, but seasoned with SALT.</p>
+      </div>
+      <button className="flex flex-center">Subscribe Now!</button>
+    </div>
+  );
+};
